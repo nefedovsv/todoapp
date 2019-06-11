74a68c8343a03f1c7bd0859e1d3cd9d3d20d180b
nefedovsv <47773817+nefedovsv@users.noreply.github.com>
2019-06-11 11:06:29 +0300

    I added some dependents  in project (body-parser,express,mongodb) and i added the file (server.js)
    I added some dependents  in project (body-parser,express,mongodb) and i added the file (server.js)

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
-var express = require('express')
-var app = express()
+const MongoClient = require('mongodb').MongoClient
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-})
+const url = 'mongodb://localhost:27017/'
+const mongoClient = new MongoClient(url, { useNewUrlParser: true })
+//let user = { name: 'Bob1', age: 334 }
+mongoClient.connect(function(err, client) {
+  const db = client.db('sergey1')
+  const collection = db.collection('users1')
+  //let user = { name: 'Tom', age: 23 }
+  /* collection.insertOne(user, function(err, results) {
+    console.log(results.ops)
+    client.close()
+  })*/
+  if (err) return console.log(err)
 
-app.listen(8000, function() {
-  console.log('Example app listening on port 8000!')
+  collection.findOneAndUpdate({ age: 21 }, { $set: { age: 25 } }, function(
+    err,
+    result
+  ) {
+    console.log(result)
+    client.close()
+  })
 })
