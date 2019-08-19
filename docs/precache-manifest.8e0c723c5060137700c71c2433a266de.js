6924223cc130cf623497507209fc57c9b581db3a
nefedovsv <47773817+nefedovsv@users.noreply.github.com>
2019-08-19 09:38:48 +0300

    build -> docs

---
+++ docs/precache-manifest.8e0c723c5060137700c71c2433a266de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8565502ea345bce089b2bf3d20568200",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "437f1a414235d2a8131b",
+    "url": "/todoapp/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "2438acf13432872e7700",
+    "url": "/todoapp/static/css/main.602dc529.chunk.css"
+  },
+  {
+    "revision": "437f1a414235d2a8131b",
+    "url": "/todoapp/static/js/2.13ae399e.chunk.js"
+  },
+  {
+    "revision": "2438acf13432872e7700",
+    "url": "/todoapp/static/js/main.774f7e9d.chunk.js"
+  },
+  {
+    "revision": "c6def23480a24191ed86",
+    "url": "/todoapp/static/js/runtime~main.280c0c24.js"
+  },
+  {
+    "revision": "e62db491818eecda4209d72ee6eba360",
+    "url": "/todoapp/static/media/note.e62db491.jpg"
+  }
+]);
